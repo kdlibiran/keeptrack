f10b1e9fc17df3779ea788b32cf8fb127bb87ca6
kdlibiran <kglibiran@up.edu.ph>
2024-09-02 22:14:59 +0800

    Revert "lab5"
    This reverts commit d45020eb563310f1866ef9b038f619f12f6d222f.

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,13 +1,8 @@
 import React from 'react';
-import { MOCK_PROJECTS } from './MockProjects';
+
 function ProjectsPage() {
   return (
-    <>
       <h1>Projects</h1>
-      <pre>
-        {JSON.stringify(MOCK_PROJECTS, null, ' ')}
-      </pre>
-    </>
   );
 }
 
