f78c81a61508b938c1b79a63dcbd8eab9d44ae60
kdlibiran <kglibiran@up.edu.ph>
2024-09-02 22:29:21 +0800

    lab10

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Project } from './Project';
 import ProjectCard from './ProjectCard';
-
+import ProjectForm from './ProjectForm';
 interface ProjectListProps {
   projects: Project[];
 }
@@ -10,6 +10,7 @@ function ProjectList({ projects }: ProjectListProps) {
   const items = projects.map((project) => (
     <div key={project.id} className="cols-sm">
       <ProjectCard project={project} />
+      <ProjectForm />
     </div>
   ));
   return <div className="row">{items}</div>;
